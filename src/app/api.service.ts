1df194b48b1b089d8ae80d040e539fb12aae0452
christianjose16 <josechapinx2@gmail.com>
2023-06-09 16:21:42 -0600

    change env

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -9,20 +9,20 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getImage(numero: string) {
-    const url = `https://v9oy12jy2l.execute-api.us-west-2.amazonaws.com/GetImages/${numero}`;
+    const url = `https://43b3nq0ov9.execute-api.us-west-2.amazonaws.com/Prod/GetImages/${numero}`;
     return this.http.get(url);
   }
   CompleteItem(numero: string){
-    const url = `https://v9oy12jy2l.execute-api.us-west-2.amazonaws.com/getbyid/${numero}`;
+    const url = `https://43b3nq0ov9.execute-api.us-west-2.amazonaws.com/Prod/getbyid/${numero}`;
     return this.http.get(url);
   }
   CompleteSO(numero: string){
     console.log('CompleteSO');
-    const url = `https://v9oy12jy2l.execute-api.us-west-2.amazonaws.com/getidOrder/${numero}`;
+    const url = `https://43b3nq0ov9.execute-api.us-west-2.amazonaws.com/Prod/getidOrder/${numero}`;
     return this.http.get(url);
   }
   GetInspection(numero: string){
-    const url = `https://v9oy12jy2l.execute-api.us-west-2.amazonaws.com/getdetails/${numero}`;
+    const url = `https://43b3nq0ov9.execute-api.us-west-2.amazonaws.com/Prod/getdetails/${numero}`;
     return this.http.get(url);
   }
 }
