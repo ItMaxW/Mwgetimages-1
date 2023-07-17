ce923666d3de403d93611c56d0f7ced38a443806
christianjose16 <josechapinx2@gmail.com>
2023-07-17 10:10:16 -0600

    changes

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { delay, timeout } from 'rxjs';
+import { defer, from, concatMap, concatAll, catchError, retry, takeWhile } from 'rxjs';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +12,37 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getImage(numero: string) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
     const url = `https://43b3nq0ov9.execute-api.us-west-2.amazonaws.com/Prod/GetImages/${numero}`;
-    return this.http.get(url);
+    return this.http.get(url).pipe(delay(1500));
   }
   CompleteItem(numero: string){
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
     const url = `https://43b3nq0ov9.execute-api.us-west-2.amazonaws.com/Prod/getbyid/${numero}`;
-    return this.http.get(url);
+    return this.http.get(url).pipe(delay(1500));
   }
   CompleteSO(numero: string){
     console.log('CompleteSO');
     const url = `https://43b3nq0ov9.execute-api.us-west-2.amazonaws.com/Prod/getidOrder/${numero}`;
-    return this.http.get(url);
+    return this.http.get(url).pipe(delay(1500));
   }
   GetInspection(numero: string){
     const url = `https://43b3nq0ov9.execute-api.us-west-2.amazonaws.com/Prod/getdetails/${numero}`;
-    return this.http.get(url);
+    const timeoutValue = 90000;
+  
+    return this.http.get(url).pipe(
+      retry(3),
+      delay(1500),
+      timeout(timeoutValue),
+      takeWhile((response: any) => response?.status !== 200),
+      catchError((error: any) => {
+        console.log('GetInspection error:', error);
+        throw error; // O cualquier otro manejo del error deseado
+      })
+      );
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ApiService } from './api.service';
+import { defer, from, concatMap, concatAll, catchError } from 'rxjs';
+import { of } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -18,13 +21,17 @@ export class AppComponent {
       (data: any) => {
         this.respuesta = data;
         this.respuesta.forEach((item: { id_trkgimg: string; id_tracking: string;   photography: string; }) => {
-          this.apiService.CompleteItem(item.id_trkgimg).subscribe((data: any) =>  {
-            // actualizar los valores correspondientes de item con la respuesta
-            item.id_trkgimg = data[0].id_trkgimg;
-            item.id_tracking = data[0].id_tracking;
-            item.photography = data[0].photography;
-
+          console.log('before');
+          this.apiService.CompleteItem(item.id_trkgimg).subscribe((datap: any) =>  {
+            setTimeout(() => {
+              console.log(datap+' dentro');
+              // actualizar los valores correspondientes de item con la respuesta
+              item.id_trkgimg = datap[0].id_trkgimg;
+              item.id_tracking = datap[0].id_tracking;
+              item.photography = datap[0].photography;
+            }, 10000);
           });
+          console.log(data+' dentro');
         });
       },
       error => {
@@ -32,30 +39,36 @@ export class AppComponent {
       }
     );
   }
+  
   onSubmitSO() {
     this.apiService.CompleteSO(this.numeroSO).subscribe(
       (data: any) => {
         this.respuesta = data;
         console.log(data);
-        this.respuesta.forEach((item: { id_trkgimg: string; id_tracking: string;   photography: string; source: number, img:string}) => {
-          if(item.source ==1){
-            this.apiService.CompleteItem(item.img).subscribe((data2: any) =>  {
-              item.id_trkgimg = data2[0].id_trkgimg;
-              item.id_tracking = data2[0].id_tracking;
-              item.photography = data2[0].photography;
+        this.respuesta.forEach((item: { id_trkgimg: string; id_tracking: string; photography: string; source: number, img:string}) => {
+          if(item.source == 1) {
+            this.apiService.CompleteItem(item.img).subscribe((data2: any) => {
+              setTimeout(() => {
+                item.id_trkgimg = data2[0].id_trkgimg;
+                item.id_tracking = data2[0].id_tracking;
+                item.photography = data2[0].photography;
+              }, 5000); 
             });
-          }else{          
-            this.apiService.GetInspection(item.img).subscribe((data3: any) =>  {
-              console.log(data3)
-            item.id_trkgimg = data3[0].id_trkgimg;
-            item.id_tracking = data3[0].id_tracking;
-            item.photography = data3[0].image;
-          });}
+          } else {
+            this.apiService.GetInspection(item.img).subscribe((data3: any) => {
+              setTimeout(() => {
+                console.log(data3);
+                item.id_trkgimg = data3[0].id_trkgimg;
+                item.id_tracking = data3[0].id_tracking;
+                item.photography = data3[0].image;
+              }, 5000);
+            });
+          }
         });
       },
       error => {
         console.log(error);
       }
     );
-  }
+  } 
 }
